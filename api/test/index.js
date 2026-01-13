31bd43391f0b18bb2fe3d3280f39053d01492120
tk42greg <42553978+gdackerm4215@users.noreply.github.com>
2026-01-13 14:56:58 -0800

    Simplify response body to 'Hello World'
    Removed Azure Storage connection details from response.

--- api/test/index.js
+++ api/test/index.js
@@ -1,21 +1,6 @@
 module.exports = async function (context, req) {
-    const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-    const containerName = process.env.AZURE_CONTAINER_NAME;
-    
     context.res = {
         status: 200,
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            message: "Test endpoint working",
-            hasConnectionString: !!connectionString,
-            connectionStringLength: connectionString ? connectionString.length : 0,
-            containerName: containerName || "not set",
-            nodeVersion: process.version
-        })
+        body: "Hello World"
     };
 };
-```
-
-Commit and push, then visit:
-```
-https://kind-stone-051bf3e1e.6.azurestaticapps.net/api/test
