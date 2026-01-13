ceb5bea191f9a310e3dd478cb9d092ecbbb1ca3d
tk42greg <42553978+gdackerm4215@users.noreply.github.com>
2026-01-13 15:11:47 -0800

    Refactor Azure storage connection handling

--- api/test/index.js
+++ api/test/index.js
@@ -1,27 +1,24 @@
-const https = require('https');
-const crypto = require('crypto');
-
 module.exports = async function (context, req) {
     try {
         const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
-        const containerName = process.env.AZURE_CONTAINER_NAME || "documents";
         
-        const config = parseConnectionString(connectionString);
-        
-        context.log('Account:', config.accountName);
-        context.log('Container:', containerName);
-        
-        const blobs = await listBlobs(config.accountName, config.accountKey, containerName);
+        const parts = {};
+        connectionString.split(';').forEach(part => {
+            const [key, ...valueParts] = part.split('=');
+            if (key && valueParts.length > 0) {
+                parts[key] = valueParts.join('=');
+            }
+        });
         
         context.res = {
             status: 200,
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 success: true,
-                accountName: config.accountName,
-                containerName: containerName,
-                blobCount: blobs.length,
-                firstFiveBlobs: blobs.slice(0, 5).map(b => b.name)
+                accountName: parts['AccountName'],
+                hasAccountKey: !!parts['AccountKey'],
+                accountKeyLength: parts['AccountKey'] ? parts['AccountKey'].length : 0,
+                endpointSuffix: parts['EndpointSuffix'] || 'core.windows.net'
             })
         };
     } catch (error) {
@@ -35,39 +32,3 @@ module.exports = async function (context, req) {
         };
     }
 };
-
-function parseConnectionString(connectionString) {
-    const parts = {};
-    connectionString.split(';').forEach(part => {
-        const [key, ...valueParts] = part.split('=');
-        if (key && valueParts.length > 0) {
-            parts[key] = valueParts.join('=');
-        }
-    });
-    return {
-        accountName: parts['AccountName'],
-        accountKey: parts['AccountKey']
-    };
-}
-
-async function listBlobs(accountName, accountKey, containerName) {
-    const date = new Date().toUTCString();
-    const version = '2020-10-02';
-    
-    const stringToSign = `GET\n\n\n\n\n\n\n\n\n\n\n\nx-ms-date:${date}\nx-ms-version:${version}\n/${accountName}/${containerName}\ncomp:list\nrestype:container`;
-    
-    const signature = createHmacSignature(accountKey, stringToSign);
-    
-    const options = {
-        hostname: `${accountName}.blob.core.windows.net`,
-        path: `/${containerName}?restype=container&comp=list`,
-        method: 'GET',
-        headers: {
-            'x-ms-date': date,
-            'x-ms-version': version,
-            'Authorization': `SharedKey ${accountName}:${signature}`
-        }
-    };
-
-    return new Promise((resolve, reject) => {
-        const req = https.re
