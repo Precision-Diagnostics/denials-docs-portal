b3ebd83250ae87a0c34beae96c888982cb1966d4
tk42greg <42553978+gdackerm4215@users.noreply.github.com>
2026-01-13 15:15:30 -0800

    Refactor Azure storage request handling

--- api/test/index.js
+++ api/test/index.js
@@ -1,7 +1,12 @@
+const https = require('https');
+const crypto = require('crypto');
+
 module.exports = async function (context, req) {
     try {
         const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+        const containerName = process.env.AZURE_CONTAINER_NAME || "documents";
         
+        // Parse connection string
         const parts = {};
         connectionString.split(';').forEach(part => {
             const [key, ...valueParts] = part.split('=');
@@ -10,15 +15,56 @@ module.exports = async function (context, req) {
             }
         });
         
+        const accountName = parts['AccountName'];
+        const accountKey = parts['AccountKey'];
+        
+        // Build request
+        const date = new Date().toUTCString();
+        const version = '2020-10-02';
+        
+        const stringToSign = `GET\n\n\n\n\n\n\n\n\n\n\n\nx-ms-date:${date}\nx-ms-version:${version}\n/${accountName}/${containerName}\ncomp:list\nrestype:container`;
+        
+        const keyBuffer = Buffer.from(accountKey, 'base64');
+        const hmac = crypto.createHmac('sha256', keyBuffer);
+        hmac.update(stringToSign, 'utf8');
+        const signature = hmac.digest('base64');
+        
+        // Make request
+        const result = await new Promise((resolve, reject) => {
+            const options = {
+                hostname: `${accountName}.blob.core.windows.net`,
+                path: `/${containerName}?restype=container&comp=list&maxresults=5`,
+                method: 'GET',
+                headers: {
+                    'x-ms-date': date,
+                    'x-ms-version': version,
+                    'Authorization': `SharedKey ${accountName}:${signature}`
+                }
+            };
+            
+            const req = https.request(options, (res) => {
+                let data = '';
+                res.on('data', chunk => data += chunk);
+                res.on('end', () => {
+                    resolve({
+                        statusCode: res.statusCode,
+                        headers: res.headers,
+                        body: data.substring(0, 1000)
+                    });
+                });
+            });
+            req.on('error', reject);
+            req.end();
+        });
+        
         context.res = {
             status: 200,
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 success: true,
-                accountName: parts['AccountName'],
-                hasAccountKey: !!parts['AccountKey'],
-                accountKeyLength: parts['AccountKey'] ? parts['AccountKey'].length : 0,
-                endpointSuffix: parts['EndpointSuffix'] || 'core.windows.net'
+                accountName: accountName,
+                containerName: containerName,
+                response: result
             })
         };
     } catch (error) {
